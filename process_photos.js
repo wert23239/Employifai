efd520038b51908a434662e979b893c438187bb9
wert23239 <lambe168@msu.edu>
2016-02-27 17:32:08 -0500

    "Changed to "

--- process_photos.js
+++ process_photos.js
@@ -1,19 +1,30 @@
 
 var Queue = require('firebase-queue'),
     Firebase = require('firebase');
-var queueRef = new Firebase('https://team-red.firebaseio.com/queue');
-var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
-    // Read and process task data
-    console.log(data);
 
-    // Do some work
-    ref = new Firebase("https://team-red.firebaseio.com/");
-    ref.push({'foo': data});
-    ref.push({'foo': "fuck.me"});
-    progress(50);
-    console.log("asfok");
-    // Finish the job asynchronously
-    setTimeout(function() {
-        resolve();
-    }, 1000);
-});
+var queueRef = new Firebase('https://team-red.firebaseio.com/queue');
+
+queueRef.child("tasks").on("child_added", function (entrySnap) {
+    var entry = entrySnap.val();
+    console.log(entry);
+
+    // Do api calls, then eventually, write to result/
+    entrySnap.ref().child("result").set({bad: ["one", "two"],warning: "asd"});
+    //entrySnap.ref().child("result").set({result: true});
+})
+//var queue = new Queue(queueRef, {sanitize: false}, function(data, progress, resolve, reject) {
+//    // Read and process task data
+//    console.log(data);
+//
+//    // Do some work
+//    //ref = new Firebase("https://team-red.firebaseio.com/");
+//    //ref.push({'foo': data});
+//    //ref.push({'foo': "fuck.me"});
+//    progress(50);
+//    console.log("asfok");
+//    resolve({success: true});
+//    // Finish the job asynchronously
+//    //setTimeout(function() {
+//    //    resolve({"result": "true"});
+//    //}, 1000);
+//});
