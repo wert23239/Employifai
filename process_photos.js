42020c948163d6763a89d6ebc8df6a896aace6b4
wert23239 <lambe168@msu.edu>
2016-02-27 16:17:27 -0500

    "task_worker"

--- process_photos.js
+++ process_photos.js
@@ -1,1 +1,19 @@
-var myFirebaseRef = new Firebase("https://team-red.firebaseio.com/");
+
+var Queue = require('firebase-queue'),
+    Firebase = require('firebase');
+var queueRef = new Firebase('https://team-red.firebaseio.com/queue');
+var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
+    // Read and process task data
+    console.log(data);
+
+    // Do some work
+    ref = new Firebase("https://team-red.firebaseio.com/");
+    ref.push({'foo': data});
+    ref.push({'foo': "fuck.me"});
+    progress(50);
+    console.log("asfok");
+    // Finish the job asynchronously
+    setTimeout(function() {
+        resolve();
+    }, 1000);
+});
